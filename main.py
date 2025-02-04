65868529c38e61a0c76ae1896eef5b12c63bb7db
AzureAD\MelissaGalang <melissa.galang@solx.ph>
2025-02-04 14:52:24 +0800

    Merge branch 'main' into feat/squareroot

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 from squareroot import sqrt
 from fibonacci import fibonacci
+from squareroot import sqrt
 
 def main():
     print('New repo')
     print(fibonacci(10))
     print(sqrt(4))
-
 
 if __name__ == '__main__':
   main()
