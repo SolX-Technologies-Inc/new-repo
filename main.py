b3cfe3b27d4e43813fd44c3f69d183fb3dcfbfae
AzureAD\JhoanaSuelo <jhoana.suelo@solx.ph>
2025-02-04 14:35:29 +0800

    chore: Added fibonacci sequence to main.py

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 from squareroot import sqrt
-
+from fibonacci import fibonacci
 
 def main():
     print('New repo')
+    print(fibonacci(10))
     print(sqrt(4))
+
 
 if __name__ == '__main__':
   main()
